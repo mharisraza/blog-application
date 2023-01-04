3b096887ff20b5299a5c84d33b1b10df41cb0a48
Muhammad Haris <wannaheadshot5@gmail.com>
2023-01-05 03:06:09 +0500

    initialized frontend

---
+++ frontend/src/components/Base.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+const Base=({title="Blog Application", children})=> {
+
+    return(
+        <>
+        <Header/>
+        {children}
+        <Footer/>
+        </>
+    );
+
+}
